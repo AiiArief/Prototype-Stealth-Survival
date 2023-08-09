cb679ee503d4d5b41ef68802c825c2f9a6d1a63c
Narendra Arief Nugraha <narendra.arief.nugraha@gmail.com>
2023-08-09 22:29:26 +0700

    refactor

--- Assets/_GameAssets/Scripts/Character.cs
+++ Assets/_GameAssets/Scripts/Character.cs
@@ -5,57 +5,52 @@ using UnityEngine;
 
 namespace SS
 {
-    [Serializable]
-    public class Need
+    public abstract class Character : MonoBehaviour
     {
-        public string Name;
-        [Range(0, 1)] public float CurrentPercentage;
-    }
+        //[SerializeField] Rigidbody2D m_rb2D;
+        //[SerializeField] Transform m_spritesParent;
 
-    [Serializable]
-    public class StatusEffect
-    {
-        public string Name;
-    }
+        protected Dictionary<string, Status> m_statusDict;
 
-    public class Character : MonoBehaviour
-    {
-        public static Character Instance;
+        protected abstract void InitStatuses();
 
-        [SerializeField] Rigidbody2D m_rb2D;
-        [SerializeField] Transform m_spritesParent;
-
-        [SerializeField] Need[] m_needs;
-        public Need[] Needs => m_needs;
-        [SerializeField] StatusEffect[] m_statusEffects;
-        public StatusEffect[] StatusEffects => m_statusEffects;
-
-        [SerializeField] float m_walkSpeed = 1.0f;
-        Vector2 m_movementInput;
-
-        private void Awake()
+        protected virtual void OnEnable()
         {
-            Instance = this;
+            StartCoroutine(_HandleStatuses());
         }
 
-        private void Update()
+        IEnumerator _HandleStatuses()
         {
-            var xInput = Input.GetAxisRaw("Horizontal");
-            var yInput = Input.GetAxisRaw("Vertical");
+            InitStatuses();
 
-            m_movementInput = new Vector2(xInput, yInput);
-
-            var timeInputPrev = Input.GetKeyUp(KeyCode.Alpha1);
-            var timeInputNext = Input.GetKeyUp(KeyCode.Alpha2);
-            var timeInput = (timeInputNext) ? 1 : (timeInputPrev) ? -1 : 0;
-            Time.timeScale = Mathf.Clamp(Time.timeScale + timeInput, 0, 2);
-
+            while(true)
+            {
+                // update semua status
+                yield return new WaitForSeconds(1.0f);
+            }
         }
 
-        private void FixedUpdate()
-        {
-            m_rb2D.velocity = m_movementInput.normalized * m_walkSpeed;
-            m_spritesParent.rotation = m_movementInput.magnitude > 0 ? Quaternion.Euler(0, 0, Mathf.Atan2(m_movementInput.y, m_movementInput.x) * (180 / Mathf.PI)) : m_spritesParent.rotation;
-        }
+        //[SerializeField] float m_walkSpeed = 1.0f;
+        //Vector2 m_movementInput;
+
+        //private void Update()
+        //{
+        //    var xInput = Input.GetAxisRaw("Horizontal");
+        //    var yInput = Input.GetAxisRaw("Vertical");
+
+        //    m_movementInput = new Vector2(xInput, yInput);
+
+        //    var timeInputPrev = Input.GetKeyUp(KeyCode.Alpha1);
+        //    var timeInputNext = Input.GetKeyUp(KeyCode.Alpha2);
+        //    var timeInput = (timeInputNext) ? 1 : (timeInputPrev) ? -1 : 0;
+        //    Time.timeScale = Mathf.Clamp(Time.timeScale + timeInput, 0, 2);
+
+        //}
+
+        //private void FixedUpdate()
+        //{
+        //    m_rb2D.velocity = m_movementInput.normalized * m_walkSpeed;
+        //    m_spritesParent.rotation = m_movementInput.magnitude > 0 ? Quaternion.Euler(0, 0, Mathf.Atan2(m_movementInput.y, m_movementInput.x) * (180 / Mathf.PI)) : m_spritesParent.rotation;
+        //}
     }
 }
